24e8143cf33a4db15154eb9bcbbaac5824952395
Ridwan Hasanah <sun.riseagain@yahoo.com>
2017-08-15 22:49:12 +0700

    Menambahkan Pagination dan file css utk menampilkan angka halaman atau page yang tersedia.

--- agcmovie.php
+++ agcmovie.php
@@ -12,9 +12,19 @@ add_shortcode('agc-movie', 'rh_agc_movie_index' );
 
 function rh_agc_movie_index(){
 
+	$uri = trim($_SERVER['REQUEST_URI'],'/');
+	$page = 1;
+	if (strpos($uri, 'page/') !== false) {
+		
+		$page = end(explode('/', $uri));
+
+	}
+
 	$parameters = array();
 
 	$parameters[] = 'with_genres=18';
+	$parameters[] = 'primary_relese_year=2017';
+	$parameters[] = 'page={page}';
 	$parameters[] = 'include_adult=false';
 	$parameters[] = 'sort_by=popularity.desc';
 
@@ -28,14 +38,14 @@ function rh_agc_movie_index(){
 	$api_key = 'd5dbdf1e3e3de7f364230240dcea83ee';
 
 	$source_url = 'https://api.themoviedb.org/3/discover/movie?api_key={api-key}&{parameters}';
-	$source_url = str_replace(array('{api-key}','{parameters}'), array($api_key, $parameters), $source_url);
+	$source_url = str_replace(array('{api-key}','{parameters}'), array($api_key, $params), $source_url);
 
 	$data = json_decode(file_get_contents($source_url) );
 	$poster_url_template = 'http://image.tmdb.org/t/p/original{file-name}?w=100';
+	//gunakan link ini http://i0.wp.com/image.tmdb.org/t/p/original{file-name}?w=100 jika ingin menggunkan jetpack agar lebih cepat
 	echo '<pre>';
 	//print_r($data);
 	echo '</pre>';
-	//gunakan link ini http://i0.wp.com/image.tmdb.org/t/p/original{file-name}?w=100 jika ingin menggunkan jetpack agar lebih cepat
 
 	$total_pages = $data->total_pages;
 
@@ -49,15 +59,40 @@ function rh_agc_movie_index(){
 		if (!empty($poster)) {
 			$image_url = str_replace('{file-name}', $poster, $poster_url_template);
 			$html     .= '<span styel="nargin: 10px;">'.
-						 '<a href="'.get_home_url().'movie'.$id.'-'.sanitize_title($title).'">'.
+						 '<a href="'.get_home_url().'/movie-'.$id.'-'.sanitize_title($title).'">'.
 						 '<img src="'.$image_url.'" width="100" height="150" alt="'.$title.'" title="'.$title.'"/>'.
 						 '</a></span>';
 		}
 	}
-	
+
 	$html .= '<div style="clear:both"></div>';
+
+	$pages = array();
+	for ($i=1; $i<=$total_pages; $i++){
+		if ($i == $page) {
+			$pages[] = '<a class="active" href="'.get_home_url().'/page/'.$i.'">'.$i.'</a>';
+
+		}else {
+		$pages[] = '<a href="'.get_home_url().'/page/'.$i.'">'.$i.'</a>';
+
+
+		}
+	}
+
+	if (!empty($pages)) {
+		$nav = '<div class="rh-pagination">'.implode('', $pages).'</div>';
+		$html .= '<hr/>'.$nav;
+	}
 	
 	return $html;
 }
 
+
+/*======= CSS Include Start =========*/
+add_action('wp_head', 'rh_agc_movie_pagination_style' );
+function rh_agc_movie_pagination_style(){
+
+	echo '<link href="'.plugin_dir_url(__FILE__ ).'style.css" rel="stylesheet" type="text/css">';
+}
+/*======= CSS Include End =========*/
 ?>
